6ddc71e752cd5a4db8b768a78a53ff35a0b4b269
lindabarbaratg <lindabarbaratg@gmail.com>
2024-12-10 21:18:52 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import csv
+
+
+
+def web_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--verbose")
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--headless')
+    options.add_argument("--disable-gpu")
+    # options.add_argument("--window-size=1920, 1200")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+driver = web_driver()
+driver.maximize_window()
+
+
+with open("x.csv", "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for line in csv_reader:
+        kw = line[0]
+        try:
+
+        
+        
+            driver.get("https://www.wikiart.org/en/edit/john-constable/new")
+            time.sleep(5)
+
+            driver.get("https://www.wikiart.org/en/edit/john-constable/new")
+            time.sleep(6)
+
+
+        
+        except:
+            print("TERJADI EROOR")
